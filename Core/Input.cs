7ca2445df1437d6c2efa7b255b482463e1110778
Steven Scoville <SSCOVI8473@my.sullivan.edu>
2026-01-15 20:40:30 -0600

    feat: implement integer input validation helpers

--- Core/Input.cs
+++ Core/Input.cs
@@ -6,22 +6,42 @@ namespace UtilitySuite.Core
 {
     public static class Input
     {
-        public static int GetInt (string prompt, int min)
+        public static int GetInt(string prompt, int min)
+        {
+            int inputVal;
+            Console.Write(prompt);
+            string intInput = Console.ReadLine();
+            intInput = intInput.Trim();
+            while (!Int32.TryParse(intInput, out inputVal) || inputVal < min)
+            {
+                Console.Write($"Invalid input. {prompt}");
+                intInput = Console.ReadLine();
+                intInput = intInput.Trim();
+            }
+            return inputVal;
+        }
+
+        public static int GetInt(string prompt, int min, int max)
+        {
+            int inputVal;
+            Console.Write(prompt);
+            string intInput = Console.ReadLine();
+            intInput = intInput.Trim();
+            while (!Int32.TryParse(intInput, out inputVal) || inputVal < min || inputVal > max)
+            {
+                Console.Write($"Invalid input. {prompt}");
+                intInput = Console.ReadLine();
+                intInput = intInput.Trim();
+            }
+            return inputVal;
+        }
+
+        public static int GetDecimal(string prompt, decimal min)
         {
             
         }
 
-        public static int GetInt (string prompt, int min, int max)
-        {
-
-        }
-
-        public static int GetDecimal (string prompt, decimal min)
-        {
-
-        }
-
-        public static int GetNonEmptyString (string prompt, string userInput)
+        public static int GetNonEmptyString(string prompt, string userInput)
         {
             
         }
